13ceb2677b0e13e0663569d482c0d617fda20edc
yoshida_i <i.yoshida1124@gmail.com>
2018-09-25 00:03:02 +0900

    tmp

---
+++ frontend/src/pages/StoreTop.vue
@@ -0,0 +1,25 @@
+
+<template>
+  <div class="hello">
+    <store-header v-bind:shopInfo=shopInfo></store-header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StoreTop',
+  data () {
+    return {
+      shopInfo: {
+        image: 'https://picsum.photos/1024/600?image=995',
+        name: 'ルノアール六本木店あああああああああああああああい…'
+      }
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import StoreTop from '@/pages/StoreTop'
+import StoreHeader from '@/components/StoreHeader'
 
 Vue.use(Router)
+
+Vue.component('store-header', StoreHeader)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/shops/:id',
+      name: 'shops',
+      component: StoreTop
     }
   ]
 })
